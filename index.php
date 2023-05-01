63d9025943e98434f1aa58b2c298a299836549c9
Mh_Shamey <mh.shamey@gmail.com>
2023-05-01 08:24:54 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+//  print ("helooo");
+// echo "asd";
+
+# variables
+// $name='ahmad';
+// $age=19;
+// echo $name;
+// echo "<br>";
+// echo $age;
+
+// echo "<br>";
+
+// echo "your name is: ".$name." and your age is :".$age;
+
+//  string  "" , ''
+# integer - , +
+# float 0.4 , 12, 0.333333
+# double 0.4 ,12 0.765767567567
+# boolean , true , false // 0 , 1
+# array 
+// $num1 =10;
+// $num2=20;
+// $is_gt=$num1<$num2;
+// echo $is_gt;
+
+// logic opertator
+
+# > < >= <= == === != !
+// $name='ahmad';
+// $bool=! '1'!==1;
+
+// echo $bool;
+
+
+// + - * / % ++ -- += -= *= /=
+// $num1=5;
+// $num2=11;
+// $num1/=10;
+// echo $num1;
+$name="ewew";
+if($name=='ahmad'){
+    echo "welcome";
+}elseif($name=='ali'){
+echo "Welcome Ali";
+}elseif($name=='ahmad'){
+    echo "Welcome My frind Fhd";
+    }else{
+  echo "not welcome"; 
+}
+
+
+echo "end if";
